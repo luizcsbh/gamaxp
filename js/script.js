6711cac7005557f15918ac103f7d3e52339c4fb9
Luiz Santos <luizcsbh@gmail.com>
2020-06-24 23:56:25 -0300

    Alteração do projeto

--- js/script.js
+++ js/script.js
@@ -24,49 +24,12 @@ $(function(){
             ativa(this);
     });
     
-    $("#aDepoimentos").on("click", function(){
-            $("#Depoimentos").scrollView();
-            ativa(this);
-    });
-    
-    $("#aContato").on("click", function(){
-            $("#Contato").scrollView();
-            ativa(this);
-    });
     
     $(window).on("scroll",function(){
         posDoc=$(document).scrollTop();
         posHome=$("#Home").offset().top-100;
         posIntegrantes=$("#Integrantes").offset().top-100;
-        posDepoimentos=$("#Depoimentos").offset().top-100;
-        posContato=$("#Contato").offset().top-100;
         
         console.log(posDoc+"/"+posIntegrantes);
-        
-        if(posDoc>=posContato){
-            ativa("#Contato");
-        }
-        else if (posDoc>=posDepoimentos){
-            ativa("#aDepoimentos");
-           
-        } 
-        else if (posDoc>=posIntegrantes){
-            ativa("#aIntegrantes");
-        } 
-        else{
-            ativa("#aHome");
-        }
     });
-    
-    $(window).on("scroll",function(){
-        posDoc=$(document).scrollTop();
-        posMedia=posDoc+$(window).height()/2;
-        posImg=$("#html5").offset().top-60;
-        if (posMedia>=posImg)
-            $("#html5").addClass("entrada");  
-    });
-     
 });
-  
-   
- 
