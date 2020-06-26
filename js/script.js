73d44d01ee16216cd48fdaa55f2e023926806ee7
Luiz Santos <luizcsbh@gmail.com>
2020-06-26 18:29:48 -0300

    Alteração do projeto e layout

--- js/script.js
+++ js/script.js
@@ -1,35 +1,28 @@
 $(function(){
-    $("a").click(function(){
-        $("a").removeClass("selected");
-        $(this).addClass("selected");
-    });
-        
+
     $.fn.scrollView=function(){
         topElem=$(this).offset().top;
-            $("html,body").animate({scrollTop:topElem},500);        
-    }; 
-    
+        $("html,body").animate({scrollTop:topElem},500); 
+    };
+
     function ativa(id){
         $("header nav a").removeClass("selected");
         $(id).addClass("selected");
     }
-        
-    $("#aHome").on("click", function(){
-            $("#Home").scrollView();
-            ativa(this);
+    $("#aHome").on("click",function(){
+        $("#home").scrollView();
+        ativa(this);
     });
-    
-    $("#aIntegrantes").on("click", function(){
-            $("#Integrantes").scrollView();
-            ativa(this);
+    $("#aIntegrantes").on("click",function(){
+        $("#integrantes").scrollView();
+        ativa(this);
     });
-    
-    
+
     $(window).on("scroll",function(){
         posDoc=$(document).scrollTop();
-        posHome=$("#Home").offset().top-100;
-        posIntegrantes=$("#Integrantes").offset().top-100;
-        
-        console.log(posDoc+"/"+posIntegrantes);
+        posHome=$("#home").offset().top-60;
+        posIntegrantes=$("#integrantes").offset().top-60;
+
+        console.log(posDoc+"/"+posIntegrantes)
     });
 });
